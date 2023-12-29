dc35b1fcb55037794767dc93a9a2076951640e15
Aravind Karthik <aravindkarthik96@gmail.com>
2023-12-29 14:54:26 +0530

    added logic to fetch and parse emails from Kotak, HDFC and Citi banks

--- nexpense_mail_to_csv.py
+++ nexpense_mail_to_csv.py
@@ -8,10 +8,173 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from email import message_from_bytes
+import re
 
 # If modifying these SCOPES, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
+def get_header(header_name, headers):
+    """
+    Fetches the header value for a given header name from a list of (Header, Value) tuples.
+    """
+    for name, value in headers:
+        if name.lower() == header_name.lower():
+            return value
+    return None
+
+def extract_hdfc_transaction_details(email_content):
+    details = {
+        'bank_name': 'HDFC Bank',  # Inferred from the email's context, as it's static in this case
+        'transaction_amount': None,
+        'merchant': None,
+        'date_time': None
+    }
+
+    # Pattern for the transaction amount
+    amount_pattern = r"Rs (\d+.\d+)"
+    amount_match = re.search(amount_pattern, email_content)
+    if amount_match:
+        details['transaction_amount'] = amount_match.group(1)
+
+    # Pattern for the merchant
+    merchant_pattern = r"at (.*?) on"
+    merchant_match = re.search(merchant_pattern, email_content)
+    if merchant_match:
+        details['merchant'] = merchant_match.group(1)
+
+    # Pattern for the date & time
+    date_time_pattern = r"on (\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2})"
+    date_time_match = re.search(date_time_pattern, email_content)
+    if date_time_match:
+        details['date_time'] = date_time_match.group(1)
+
+    return details
+
+def extract_kotak_transaction_details(email_content):
+    details = {
+        'bank_name': 'Kotak Mahindra Bank Ltd',  # Inferred as static from the email context
+        'transaction_amount': None,
+        'merchant': None,
+        'date_time': None
+    }
+
+    # Pattern for the transaction amount
+    amount_pattern = r"A Transaction of INR (\d+.\d+)"
+    amount_match = re.search(amount_pattern, email_content)
+    if amount_match:
+        details['transaction_amount'] = amount_match.group(1)
+
+    # Pattern for the merchant
+    merchant_pattern = r"at (.+?) on"
+    merchant_match = re.search(merchant_pattern, email_content)
+    if merchant_match:
+        details['merchant'] = merchant_match.group(1)
+
+    # Pattern for the date
+    date_pattern = r"on (\d{2}-\w{3}-\d{4})"
+    date_match = re.search(date_pattern, email_content)
+    if date_match:
+        details['date_time'] = date_match.group(1)
+
+    return details
+
+def extract_citibank_transaction_details(email_content):
+    details = {
+        'bank_name': 'Citibank',  # Known from the context
+        'transaction_amount': None,
+        'merchant': None,
+        'date_time': None
+    }
+
+    # Pattern for the transaction amount
+    amount_pattern = r"Rs\. (\d+(\.\d+)?)"
+    amount_match = re.search(amount_pattern, email_content)
+    if amount_match:
+        details['transaction_amount'] = amount_match.group(1)
+
+    # Pattern for the merchant
+    merchant_pattern = r"at ([\w\s\-]+) on"
+    merchant_match = re.search(merchant_pattern, email_content)
+    if merchant_match:
+        details['merchant'] = merchant_match.group(1).strip()
+
+    # Pattern for the date & time
+    date_pattern = r"on (\d{2}-\w{3}-\d{2})"
+    date_match = re.search(date_pattern, email_content)
+    if date_match:
+        details['date_time'] = date_match.group(1)
+
+    return details
+
+def extract_citibank_upi_transaction_details(email_content):
+    details = {
+        'bank_name': 'Citibank',  # Known from the context
+        'transaction_amount': None,
+        'merchant': None,
+        'date_time': None
+    }
+
+    # Pattern for the transaction amount
+    amount_pattern = r"Rs\. (\d+(\.\d+)?)"
+    amount_match = re.search(amount_pattern, email_content)
+    if amount_match:
+        details['transaction_amount'] = amount_match.group(1)
+
+    # Pattern for the merchant
+    merchant_pattern = r"at ([\w\s\-]+) on"
+    merchant_match = re.search(merchant_pattern, email_content)
+    if merchant_match:
+        details['merchant'] = merchant_match.group(1).strip()
+
+    # Pattern for the date & time
+    date_pattern = r"on (\d{2}-\w{3}-\d{2})"
+    date_match = re.search(date_pattern, email_content)
+    if date_match:
+        details['date_time'] = date_match.group(1)
+
+    return details
+
+def extract_citibank_upi_transaction_details_v2(email_content):
+    details = {
+        'bank_name': 'Citibank',  # Known from the context
+        'transaction_amount': None,
+        'merchant': None,
+        'date_time': None
+    }
+
+    # Adjusted pattern for the transaction amount
+    amount_pattern = r"INR (\d+(?:\.\d{1,2})?)"
+    amount_match = re.search(amount_pattern, email_content)
+    if amount_match:
+        details['transaction_amount'] = amount_match.group(1)
+
+    # Adjusted pattern for the date & time
+    date_time_pattern = r"on (\d{2}-\w{3}-\d{2,4} at \d{2}:\d{2})"
+    date_time_match = re.search(date_time_pattern, email_content)
+    if date_time_match:
+        details['date_time'] = date_time_match.group(1)
+
+    # Adjusted pattern for merchant/UPI ID
+    merchant_pattern = r"and account (.*?) has been credited"
+    merchant_match = re.search(merchant_pattern, email_content)
+    if merchant_match:
+        details['merchant'] = merchant_match.group(1).strip()
+
+    return details
+
+    return details
+
+def get_mime_message(service, user_id, msg_id):
+    try:
+        message = service.users().messages().get(userId=user_id, id=msg_id, format='raw').execute()
+        msg_raw = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
+        mime_msg = email.message_from_bytes(msg_raw)
+        return mime_msg
+    except Exception as error:
+        print(f'An error occurred: {error}')
+        return None
+        
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -26,7 +189,7 @@ class MainWindow(QMainWindow):
 
         # Connect the button to the function that will handle the email fetching
         self.button.clicked.connect(self.fetch_emails)
-
+   
     def fetch_emails(self):
         creds = None
         # The file token.json stores the user's access and refresh tokens, and is
@@ -49,23 +212,51 @@ class MainWindow(QMainWindow):
             # Call the Gmail API
             service = build('gmail', 'v1', credentials=creds)
             # Request a list of all the messages
-            results = service.users().messages().list(userId='me', labelIds = [],maxResults=100).execute()
+            results = service.users().messages().list(userId='me', labelIds = ["CATEGORY_UPDATES"],maxResults=100).execute()
+            
             messages = results.get('messages', [])
 
             if not messages:
                 print("No messages found.")
                 return
             print(f"found {len(messages)}")
+    
+            self.button.setDisabled = True
+            
+            for message in messages:
+                mime_msg = get_mime_message(service, 'me', message['id'])
+                if not mime_msg:
+                    continue  # if the message couldn't be retrieved, skip it
 
-            # Fetch each message and process it
-            for message in messages[:5]:  # Retrieve only the first 5 messages for demo purposes
-                msg = service.users().messages().get(userId='me', id=message['id']).execute()
-                print(msg['snippet'])
-                # You can add here more processing logic to parse the email and extract needed data
-
+                email_body = None
+                if mime_msg.is_multipart():
+                    for part in mime_msg.walk():
+                        # Check the content type and ignore attachments
+                        if part.get_content_type() in ["text/plain", "text/html"] and "attachment" not in part.get("Content-Disposition", ""):
+                            email_body = part.get_payload(decode=True).decode()
+                            break  # once the correct body is found, no need to continue looping
+                else:
+                    # if it is a simple email body, just get the payload
+                    email_body = mime_msg.get_payload(decode=True).decode()
+                
+                # Ensure we have an email body to process
+                if email_body:
+                    subject = get_header("Subject", mime_msg.items())
+                    
+                    if subject == "Alert :  Update on your HDFC Bank Credit Card" :
+                        print(extract_hdfc_transaction_details(email_body))
+                    elif subject == "Kotak Bank Credit Card Transaction Alert" :
+                        print(extract_kotak_transaction_details(email_body))
+                    elif subject == "Citi Purchase Alert" :
+                        print(extract_citibank_transaction_details(email_body))
+                    elif subject == "CitiAlert - UPI Transaction Successful" :
+                        print(extract_citibank_upi_transaction_details(email_body))
+                    elif subject == "CitiAlert - UPI Fund Transfer Acknowledgement" :
+                        print(extract_citibank_upi_transaction_details_v2(email_body))
+                        
+            print("Completed Fetching")
         except Exception as error:
             print(f'An error occurred: {error}')
-
 
 # Initialize the application and create the GUI
 app = QApplication(sys.argv)
